8a810005c9980b2a7de357a833927db5509f6e21
guiguihao <393314661@qq.com>
2017-09-27 14:47:15 +0800

    Initial Commit

---
+++ h5correspondence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  h5correspondence
+//
+//  Created by jiangguishun1 on 2017/9/27.
+//  Copyright © 2017年 jiangguishun1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ h5correspondence/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  h5correspondence
+//
+//  Created by jiangguishun1 on 2017/9/27.
+//  Copyright © 2017年 jiangguishun1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
